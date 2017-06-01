930423ca4a943ce1905ab73ab5f9e1b1b9546e73
insun <insun@IS_Surface>
2017-06-01 15:25:05 +0900

    first

--- JobTest_miraDH/src/test4.java
+++ JobTest_miraDH/src/test4.java
@@ -5,7 +5,7 @@ public class test4 {
 	    
 		for(int i=1; i<=10; i++){
 			for(int j=1; j<=10; j++){
-		       System.out.print(i*j + " "); // 1단부터 출력
+		       System.out.print(i*j + " "); // 1단부터 출력1
 			}
 			   System.out.println();  // 해당 단이 끝나면 줄바꿈111
 		}
